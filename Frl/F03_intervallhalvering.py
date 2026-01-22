ec24e013438c98067b217c3c80a3d32281f2fbe0
Joar Bagge <joar.bagge@gmail.com>
2026-01-22 13:08:49 +0100

    Uppdaterat exempel från Frl 3

--- Frl/F03_intervallhalvering.py
+++ Frl/F03_intervallhalvering.py
@@ -30,7 +30,19 @@ def main():
     i = 0 # räknare
     print(i, x, fx, felgrans)
     while felgrans >= feltolerans:
-        break # TODO: fyll på kod här!
+        if fa*fx < 0: # olika tecken
+            (a, b) = (a, x)
+            (fa, fb) = (fa, fx)
+        elif fx == 0: # vi har hittat roten!
+            break
+        else: # fx*fb < 0, dvs behåll [x,b]
+            (a, b) = (x, b)
+            (fa, fb) = (fx, fb)
+        x = (a+b)/2
+        fx = f(x)
+        felgrans = (b-a)/2
+        i += 1
+        print(i, x, fx, felgrans)
     return
 
 
