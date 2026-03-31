2c33ea3ef40a2fa29c5d66bf2f4c9fa356665769
Joar Bagge <joar.bagge@gmail.com>
2026-03-31 17:11:03 +0200

    Uppdaterat exempel från Frl 12

--- Frl/F12_optimering.py
+++ Frl/F12_optimering.py
@@ -12,7 +12,8 @@ def main():
     x2 = np.linspace(-2, 2)
     X1, X2 = np.meshgrid(x1, x2)
     f = X1**4 + X2**4 + 2*X1**2*X2**2 + 6*X1*X2 - 4*X1 - 4*X2 + 1
-    plt.contourf(X1, X2, f)
+    plt.clf()
+    plt.contourf(X1, X2, f, levels=np.arange(-10, 1))
     #plt.pcolormesh(X1, X2, f)
     plt.colorbar()
 
@@ -26,10 +27,47 @@ def main():
         [8*x1*x2 + 6, 12*x2**2 + 4*x1**2],
     ])
 
-    # TODO
+    # Startgissning
+    x = np.array([1.0, -0.5])
+
+    print('\nNewton')
+    diff = np.array([np.inf, np.inf])
+    tol = 1e-10
+    i = 0
+    while np.linalg.norm(diff) > tol:
+        x1, x2 = x
+        diff = -np.linalg.solve(H(x1,x2), gradf(x1,x2))
+        x += diff
+        i += 1
+        plt.plot(x1, x2, 'yo')
+        print(i, x, np.linalg.norm(diff))
+
+    # Plotta lösningen
+    x1, x2 = x
+    plt.plot(x1, x2, 'r*')
+    plt.title('Funktionen $f$ och dess minimum')
 
     # Gradientsökning
-    # TODO
+    alfa = 0.05
+
+    # Startgissning
+    x = np.array([1.0, -0.5])
+
+    print('\nGradientsökning')
+    diff = np.array([np.inf, np.inf])
+    tol = 1e-10
+    i = 0
+    while np.linalg.norm(diff) > tol:
+        x1, x2 = x
+        diff = -alfa * gradf(x1,x2)
+        x += diff
+        i += 1
+        plt.plot(x1, x2, 'bo')
+        print(i, x, np.linalg.norm(diff))
+
+    # Plotta lösningen
+    x1, x2 = x
+    plt.plot(x1, x2, 'g*')
 
     return
 
